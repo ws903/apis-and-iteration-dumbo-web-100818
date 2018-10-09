5fb31e719937bd85adb6cd705d63d0da5237a618
ws903 <ws903@nyu.edu>
2018-10-09 14:28:01 -0400

    people pages iteration

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,20 +2,22 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-
-
 def get_character_movies_from_api(character)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
-  # This is done for educational purposes. This is not typically done in code.
 
-  response_hash["results"].each do |features|
-    if features["name"] == character
-      return films_arr = features["films"].sort
-    end
+  n = 1
+  while response_hash["next"] != nil
+    response_string = RestClient.get("https://swapi.co/api/people/?page=#{n}")
+    response_hash = JSON.parse(response_string)
+
+    response_hash["results"].each { |features|
+      if features["name"] == character
+        return films_arr = features["films"].sort
+      end
+    }
+  n+=1
   end
 
   # iterate over the response hash to find the collection of `films` for the given
@@ -31,12 +33,12 @@ end
 
 def print_movies(films_arr)
   # some iteration magic and puts out the movies in a nice list
-  films_arr.each_with_index do |film_link, index|
+  films_arr.each_with_index { |film_link, index|
     film_response_string = RestClient.get(film_link)
     film_response_hash = JSON.parse(film_response_string)
-    
+
     puts "#{index+1} #{film_response_hash["title"]}"
-  end
+  }
 end
 
 def show_character_movies(character)
