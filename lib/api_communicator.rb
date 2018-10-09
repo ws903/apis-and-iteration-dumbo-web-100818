cc8b12ba5a1232a1925f42742e591837afb6ae30
ws903 <ws903@nyu.edu>
2018-10-09 15:22:32 -0400

    star wars working

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,7 +3,6 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character)
-  #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
 
@@ -19,16 +18,6 @@ def get_character_movies_from_api(character)
     }
   n+=1
   end
-
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
 end
 
 def print_movies(films_arr)
@@ -47,6 +36,5 @@ def show_character_movies(character)
 end
 
 ## BONUS
-
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
